e8566b512aa8740586388b30c952f8478a559198
Faizaan Anche <faizaananche@Faizaan-ka-laipatopa.local>
2024-06-10 10:51:46 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import diceIcon from "./images/icon-dice.svg"
+import divider from "./images/pattern-divider-desktop.svg"
+import axios from 'axios';
 
 function App() {
+
+  const [advice, setAdvice] = useState("");
+  const [adviceId, setAdviceId] = useState();
+
+  useEffect(() => getAdvice);
+
+  async function getAdvice(){
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    console.log(response);
+    setAdviceId(response.data.slip.id);
+    setAdvice(response.data.slip.advice);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <p className="advice-id">ADVICE #{adviceId}</p>
+        <p className='advice'>"{advice}"</p>
+        <button className="circular-button" onClick={getAdvice}>
+            <img src={diceIcon} alt="dice"/>
+        </button>
+        <img src={divider} alt="divider" className='divider'/>
+      </div>
     </div>
   );
 }
